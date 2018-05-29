6ba71c84d24a0a6196af8294f08dd26a5574b8d2
Skylar Thompson <skylar.thompson@gmail.com>
2018-05-29 11:19:01 -0500

    sort by incidence count

--- extract_wsrid_counts.sh
+++ extract_wsrid_counts.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
-# Accepts hail dataset over STDIN, prints unique list of WSR_IDs over STDOUT
+# Accepts hail dataset over STDIN, prints unique list of WSR_IDs over STDOUT sorted by incidence count
 
 # Example exection: aprun -q -n 1 -d 1 -N 1 ./extract_wsrid_counts.sh < /projects/training/baqk/hail-2017.csv > ~/scratch/wsir_counts.txt
 
 cut -d, -f4 \
-    |  uniq -cd
+    | uniq -cd \
+    | sort -nr
