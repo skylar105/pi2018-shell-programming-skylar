742bce37701f93e5c5a4ae3c15e559b2979e4f35
Skylar Thompson <skylar.thompson@gmail.com>
2018-05-29 11:28:31 -0500

    extract_wsrid_counts can run in parallel

--- extract_wsrid_counts.sh
+++ extract_wsrid_counts.sh
@@ -2,7 +2,7 @@
 
 # Accepts hail dataset over STDIN, prints unique list of WSR_IDs over STDOUT sorted by incidence count
 
-# Example exection: aprun -q -n 1 -d 1 -N 1 ./extract_wsrid_counts.sh < /projects/training/baqk/hail-2017.csv > ~/scratch/wsir_counts.txt
+# Example exection: aprun -q -n 1 -d 32 -N 1 ./extract_wsrid_counts.sh < /projects/training/baqk/hail-2017.csv > ~/scratch/wsir_counts.txt
 
 NUMPROC=32
 
