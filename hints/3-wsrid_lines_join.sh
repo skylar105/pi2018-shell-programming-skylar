631b1182f328b60e85c9f26ea6a57363857fd814
Skylar Thompson <skylar.thompson@gmail.com>
2018-05-30 07:18:31 -0500

    preserve field order

--- hints/3-wsrid_lines_join.sh
+++ hints/3-wsrid_lines_join.sh
@@ -14,7 +14,8 @@ i=0
 # Iterate over each command line argument and output it to STDOUT, delimited with newlines,
 # and sorted to comply with join's input requirements.
 # join will read the list of join conditions as the first file, and the hail dataset over STDIN.
-join -t, -1 1 -2 4 <(
+# Specify an output format to preserve field order (otherwise join will put the join field at the beginning)
+join -t, -o "1.1 1.2 1.3 1.4 1.5 1.6 1.7 1.8 1.9 1.10" -1 4 -2 1 - <(
     for arg; do
         # Do not output newline on the last argument, since join will interpret that as
         # an empty join field
@@ -24,4 +25,4 @@ join -t, -1 1 -2 4 <(
             echo "${arg}"
         fi
         i=$((i+1))
-    done|sort) -
+    done|sort)
